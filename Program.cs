f135ab709f1c3738a21c6f87d75dde0907ca470e
Andrey Kerges Nascimento <andrey.nascimento2@fatec.sp.gov.br>
2024-09-03 20:41:46 -0300

    Exercício Finalizado

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int num1, num2;
+Console.WriteLine("--- Divisão exata/resto ---");
+
+
+Console.WriteLine("Entre com o primeiro número: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Entre com o segundo número: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % num2 == 0)
+{
+    Console.WriteLine("Divisão Exata");
+}
+else
+{
+    Console.WriteLine("Divisão Não Exata");
+}
+
